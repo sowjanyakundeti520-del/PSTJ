80832bf8ad76ca825cdaef9eef021c24955e8ad0
sowjanyakundeti520-del <sowjanyakundeti520@gmail.com>
2026-07-27 22:33:27 +0530

    Add files via upload

---
+++ Twoarguments.java
@@ -0,0 +1,11 @@
+class Twoarguments {
+
+    static void add(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+
+    public static void main(String[] args) {
+        add(10, 20);  
+    }
+}
